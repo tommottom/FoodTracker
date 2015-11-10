99e7b16e8114406a8395b1bc8c38c52210e1cca8
tommottom <fb53ice4j@gmail.com>
2015-11-10 17:01:33 +0900

    Finish UI to Code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK：プロパティ
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK：アクション
 
-
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+         mealNameLabel.text = "Default Text"
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        // User chaege TextField note
+        mealNameLabel.text = textField.text
+    }
 }
 
