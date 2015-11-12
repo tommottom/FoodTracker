1dd21a1b97277a97ab35765964f98e2838208842
tommottom <fb53ice4j@gmail.com>
2015-11-12 19:47:52 +0900

    note appear view cell

---
+++ FoodTrackerTests/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by 高橋智貴 on 2015/11/12.
+//  Copyright © 2015年 高橋智貴. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
